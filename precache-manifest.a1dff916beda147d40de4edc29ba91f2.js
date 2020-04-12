9075d42dafc06c3b3260da11a3c84259ceab20cb
OtacilioN <osmn@cesar.org.br>
2020-04-12 01:27:37 -0300

    Updates

---
+++ precache-manifest.a1dff916beda147d40de4edc29ba91f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fdb61843773fb452ddcaa33f0c3365",
+    "url": "/simulador-risco-energia/index.html"
+  },
+  {
+    "revision": "64d0930121663b34a656",
+    "url": "/simulador-risco-energia/static/css/main.3ee3c8a8.chunk.css"
+  },
+  {
+    "revision": "39b56cc374ef8b6e0639",
+    "url": "/simulador-risco-energia/static/js/2.0039c8cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/simulador-risco-energia/static/js/2.0039c8cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d0930121663b34a656",
+    "url": "/simulador-risco-energia/static/js/main.354187b7.chunk.js"
+  },
+  {
+    "revision": "add4344631533387e37c",
+    "url": "/simulador-risco-energia/static/js/runtime-main.68276d3e.js"
+  }
+]);
